c1a7091bb062a584004bd90c8330469b770f3010
John Nguyen <ipwnponies@gmail.com>
2025-05-01 23:07:54 -0700

    Remove logic to get self ip address
    Not needed, since geolocation ip does not take input and assumes this
    value.

--- functions/weather.location.fish
+++ functions/weather.location.fish
@@ -1,10 +1,4 @@
 function weather.location -d "Get the current geographic location"
-  # Attempt to get our external IP address.
-  if not set ip (__weather_get_ip)
-    echo "No Internet connection or IP service unavailable."
-    return 1
-  end
-
   # Fetch location data based on our IP
   if not set geoip_data (weather.fetch "https://freeip2geo.net/api")
     echo "Unable to query GeoIP data; please try again later."
@@ -16,27 +10,4 @@ function weather.location -d "Get the current geographic location"
   echo $geoip_data | jq '.longitude'
   echo $geoip_data | jq -r '.city?'
   echo $geoip_data | jq -r '.country_name'
-end
-
-function __weather_get_ip -d "Get the current device's public IP address"
-  # Attempt to get our external IP using the OpenDNS resolver.
-  if type -q dig
-    if set -q __weather_system_dns
-      if set ip (dig +short myip.opendns.com 2>/dev/null)
-        echo $ip
-        return
-      end
-    else
-      if set ip (dig +short myip.opendns.com @resolver1.opendns.com 2>/dev/null)
-        echo $ip
-        return
-      end
-    end
-  end
-
-  # Attempt to get our external IP using a web service.
-  if set ip (weather.fetch "http://ipecho.net/plain")
-    echo $ip
-  end
-
 end
